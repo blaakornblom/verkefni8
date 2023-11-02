d595f9d423a13e94f4b7439819b24cfd94dfbab6
blaakornblom <yih2@hi.is>
2023-11-02 21:57:42 +0000

    sdshuaid

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { createCartLine, showCartContent } from './lib/ui.js';
+import { createCartLine, showCartContent } from "./lib/ui.js";
+import { formatPrice } from "./lib/helpers.js";
 
 /**
  * @typedef {Object} Product
@@ -6,76 +7,37 @@ import { createCartLine, showCartContent } from './lib/ui.js';
  * @property {string} title Titill vöru, ekki tómur strengur.
  * @property {string} description Lýsing á vöru, ekki tómur strengur.
  * @property {number} price Verð á vöru, jákvæð heiltala stærri en 0.
- * 
+ *
  */
 
 const products = [
   {
     id: 1,
-    title: 'HTML húfa',
+    title: "HTML húfa",
     description:
-      'Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.',
+      "Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.",
     price: 5_000,
   },
   {
     id: 2,
-    title: 'CSS sokkar',
-    description: 'Sokkar sem skalast vel með hvaða fótum sem er.',
+    title: "CSS sokkar",
+    description: "Sokkar sem skalast vel með hvaða fótum sem er.",
     price: 3_000,
   },
   {
     id: 3,
-    title: 'JavaScript jakki',
-    description: 'Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.',
+    title: "JavaScript jakki",
+    description: "Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.",
     price: 20_000,
   },
 ];
 
-/**
- * Bæta vöru í körfu
- * @param  {Product} product
- * @param {number} quantity 
- */
-function addProductToCart(product, quantity) {
-  const cartTableBodyElement = document.querySelector('.cart table tbody');
-
-  const existingLine = cartTableBodyElement.querySelector(`tr[data-product-id="${product.id}"]`);
-  
-  if (!cartTableBodyElement) {
-    console.warn('fann ekki .cart table');
-    return;
-  }
-  
-  if (existingLine) {
-
-    const currentQuantity = parseInt(existingLine.dataset.quantity, 10);
-    existingLine.dataset.quantity = currentQuantity + quantity;
-
-
-    const quantityElement = existingLine.querySelector('.quantity');
-    if (quantityElement) {
-      quantityElement.textContent = existingLine.dataset.quantity;
-    }
-    
-  } else {
-
-    const cartLine = createCartLine(product, quantity);
-    cartTableBodyElement.appendChild(cartLine);
-  }
-
-
-  showCartContent(true);
-
-  updateTotalPrice();
-
-}
-
 function submitHandler(event) {
   // prevent submitting forms
   event.preventDefault();
-  
+
   // find the next tr element
-  const parent = event.target.closest('tr');
+  const parent = event.target.closest("tr");
 
   const productId = Number.parseInt(parent.dataset.productId);
 
@@ -87,15 +49,10 @@ function submitHandler(event) {
   }
 
   // the command in order to find the total amount of input
-  const quantityInputElement = parent.querySelector('input');
-
-
+  const quantityInputElement = parent.querySelector("input");
 
   // on input
   const quantity = Number.parseInt(quantityInputElement.value);
-
-  
-
 
   // add to cart
   addProductToCart(product, quantity);
@@ -105,72 +62,98 @@ function submitHandler(event) {
   // new price shown as total price
   element.textContent = `${element} kr.-`;
 
-  updateAmount();
+  updateTotalPrice();
+
+  if ((document.querySelector('.tableThing').style.display === "none")) {
+    console.log("gafjeao");
+    document.querySelector('.tableThing').style.display = "block";
+  }
+}
+
+/**
+ * Bæta vöru í körfu
+ * @param  {Product} product
+ * @param {number} quantity
+ */
+function addProductToCart(product, quantity) {
+  const cartTableBodyElement = document.querySelector(".cart table tbody");
+  const existingLine = cartTableBodyElement.querySelector(
+    `tr[data-product-id="${product.id}"]`
+  );
+
+  if (!cartTableBodyElement) {
+    console.warn("fann ekki .cart table");
+    return;
+  }
+
+  if (existingLine) {
+    const currentQuantity = parseInt(existingLine.dataset.quantity, 10);
+    existingLine.dataset.quantity = currentQuantity + quantity;
+
+    const quantityElement = existingLine.querySelector(".foo")
+    const totalPriceElement = existingLine.querySelector(".totalPriceOfItem");
+    if (quantityElement) {
+      quantityElement.textContent = existingLine.dataset.quantity;
+      totalPriceElement.textContent = formatPrice(existingLine.dataset.quantity * existingLine.dataset.price);
+    }
+  } else {
+    const cartLine = createCartLine(product, quantity);
+    cartTableBodyElement.appendChild(cartLine);
+  }
+
+  showCartContent(true);
   updateTotalPrice();
 }
 
 // to get all forms under the class add
-const addToCartForms = document.querySelectorAll('.add')
+const addToCartForms = document.querySelectorAll(".add");
 
 // rewrite as arrays
 for (const form of Array.from(addToCartForms)) {
   // add submit to each
-  form.addEventListener('submit', submitHandler);
+  form.addEventListener("submit", submitHandler);
 }
 
 // attach an event handler
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  
-  let state = 'form';
+document.addEventListener("DOMContentLoaded", function () {
+  let state = "form";
   const toggleButton = document.getElementById("toggleButton");
-  const formFields = document.querySelectorAll('.form-field');
-  const receiptSection = document.querySelector('.receipt');
+  const formFields = document.querySelectorAll(".form-field");
+  const receiptSection = document.querySelector(".receipt");
 
-
-  toggleButton.addEventListener("click", function(event) {
-
+  toggleButton.addEventListener("click", function (event) {
     event.preventDefault();
 
-    if (state === 'form') {
-
-      formFields.forEach(function(field) {
+    if (state === "form") {
+      formFields.forEach(function (field) {
         field.style.display = "block";
       });
-      state = 'receipt';
-    } else if (state === 'receipt') {
-
+      state = "receipt";
+    } else if (state === "receipt") {
       receiptSection.style.display = "block";
       toggleButton.style.display = "none";
     }
   });
 });
 
+export function updateTotalPrice() {
+  const totalPriceElement = document.querySelector(".priceTotal");
 
-function updateTotalPrice() {
-  const totalPriceElement = document.querySelector('.priceTotal');
-
-  const cartRows = document.querySelectorAll('.cart tbody tr');
-  let totalSum = 0;
+  const cartRows = document.querySelectorAll(".cart tbody tr");
+  let finalSum = 0;
 
   // here is the for-loop that calculates the sum
   for (const line of cartRows) {
-    const p = line.dataset.price
-    const q = line.dataset.quantity;
-    
+    const g = line.dataset.price;
+    const h = line.dataset.quantity;
 
-    totalSum += p * q;
-  };
+    finalSum += g * h;
+  }
 
   // send the result out
-  totalPriceElement.textContent = `${totalSum} kr.-`;
+  totalPriceElement.textContent = `${finalSum} kr.-`;
 }
+
+// dont know if this is needed but ill put it here for good measures
 updateTotalPrice();
-
-
-  
-
-
-
-
